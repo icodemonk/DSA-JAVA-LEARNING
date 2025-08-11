aa53149539f4660760ed89d1d4f24149e82e0ff4
Aditya pal <studymailaditya@gmail.com>
2025-08-11 19:36:43 +0530

    update on pattern practice

--- untitled/src/Printing_Pattern_loops/Simple_pattern.java
+++ untitled/src/Printing_Pattern_loops/Simple_pattern.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Simple_pattern {
 
-    static void printpattern(int a) {
+    static void printpattern1(int a) {
         for(int i=0 ; i < a;i++){
             for (int j=0;j<a;j++){
                 System.out.print("* ");
@@ -12,6 +12,96 @@ public class Simple_pattern {
             System.out.println(" ");
         }
     }
+
+    static void printpattern2(int a){
+        for (int i=0 ;i<a;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void printpattern3(int a){
+        for (int i=1;i<=a;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void printpattern4(int a){
+        for (int i=1;i<=a;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void printpattern5(int a){
+        for (int i=1;i<=a;i++){
+            for (int j=1;j<=a-i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void printpattern6(int a){
+        for (int i=1;i<=a;i++){
+            for (int j=1;j<=a-i+1;j++){
+                System.out.print( j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void printpattern7(int a){
+
+        for (int i=0;i<a;i++){
+//            space
+            for (int j=0;j< a-i-1;j++){
+                System.out.print(" ");
+            }
+//            star
+            for (int k=0;k<2*i+1;k++){
+                System.out.print("*");
+
+            }
+//            space
+                for (int j=0;j< a-i-1;j++){
+                    System.out.print(" ");
+                }
+
+            System.out.println();
+
+
+
+        }
+    }
+    static void printpattern8(int a){
+
+        for (int i=0;i<a;i++){
+//            space
+            for (int j=0;j<a-i-1;j++){
+                System.out.print(" ");
+            }
+//            star
+            for (int k=0;k<2*i+1;k++){
+                System.out.print("*");
+
+            }
+//            space
+            for (int j=0;j< a-i-1;j++){
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int val=sc.nextInt();
@@ -19,7 +109,7 @@ public class Simple_pattern {
         for (int i=0;i<val;i++){
 
             int a=sc.nextInt();
-            printpattern(a);
+            printpattern7(a);
 
         }
         sc.close();
